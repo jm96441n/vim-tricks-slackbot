31890e3a0f3186520267a652f700b56ccc39792e
jm96441n <john@jmaguire.tech>
2021-03-16 23:09:45 -0400

    Updated dockerfile/makefile, added bot code to create message and marshal into json

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.16.2-buster as setup
+FROM golang:1.16.2-buster as prd
 WORKDIR /vim-tricks/function
 # build
 ADD . .
--- slack/bot.go
+++ slack/bot.go
@@ -1,8 +1,13 @@
 package slack
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"fmt"
-	"log"
+	"io"
+	"io/ioutil"
+	"net/http"
 	"vim-tricks-slackbot/rss"
 )
 
@@ -10,21 +15,107 @@ type slackFormatter interface {
 	FormatHTMLToMarkdown(string) (string, error)
 }
 
+type httpPoster interface {
+	Post(string, string, io.Reader) (*http.Response, error)
+}
+
 // Bot handles interaction with slack
 type Bot struct {
 	formatter slackFormatter
+	client    httpPoster
 }
 
-func NewBot(formatter slackFormatter) Bot {
-	return Bot{formatter: formatter}
+// NewBot is the constructor for a bot
+func NewBot(formatter slackFormatter, client httpPoster) Bot {
+	return Bot{
+		formatter: formatter,
+		client:    client,
+	}
 }
 
+// SendMessage formats the message and sends it so slack
 func (b Bot) SendMessage(item *rss.Item) error {
-	itemDesc, err := b.formatter.FormatHTMLToMarkdown(item.Description)
+	message, err := b.buildMessage(item)
 	if err != nil {
-		log.Fatal(err)
 		return err
 	}
-	fmt.Println(itemDesc)
+	jsonMsg, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(jsonMsg))
+	resp, err := b.client.Post("localhost:8000", "application/json", bytes.NewBuffer(jsonMsg))
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != 200 {
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+		return errors.New(string(b))
+	}
 	return nil
+}
+
+var section = "section"
+var divider = "divider"
+var mkdwn = "mrkdwn"
+
+func (b Bot) buildMessage(item *rss.Item) (Message, error) {
+	m := Message{}
+	fmtTitle, err := b.formatter.FormatHTMLToMarkdown(item.Title)
+	if err != nil {
+		return m, err
+	}
+	fmtDesc, err := b.formatter.FormatHTMLToMarkdown(item.Description)
+	if err != nil {
+		return m, err
+	}
+	fmtContent, err := b.formatter.FormatHTMLToMarkdown(item.Content)
+	if err != nil {
+		return m, err
+	}
+	m.Blocks = []Section{
+		Section{
+			Type: section,
+			Text: &SubSection{
+				Type: mkdwn,
+				Text: fmtTitle,
+			},
+		},
+		Section{
+			Type: divider,
+		},
+		Section{
+			Type: section,
+			Text: &SubSection{
+				Type: mkdwn,
+				Text: fmtDesc,
+			},
+		},
+		Section{
+			Type: section,
+			Text: &SubSection{
+				Type: mkdwn,
+				Text: fmtContent,
+			},
+		},
+	}
+
+	return m, nil
+}
+
+type Message struct {
+	Blocks []Section `json:"blocks"`
+}
+
+type Section struct {
+	Type string      `json:"type"`
+	Text *SubSection `json:"text,omitempty"`
+}
+
+type SubSection struct {
+	Type string `json:"type,omitempty"`
+	Text string `json:"text,omitempty"`
 }
